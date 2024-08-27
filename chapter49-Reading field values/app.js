b7806e296684670f4cb07b7e0760801e617e1d09
MisbahAnjumShaikh <misbahanjumshaikh05pk@gmail.com>
2024-08-27 12:32:38 +0500

    Explore the chapter 49 : Reading Field Values

---
+++ chapter49-Reading field values/app.js
@@ -0,0 +1,15 @@
+function checkAddress(emailId){
+    var emailId = document.getElementById(emailId).value;
+    // Alert user to fill the field as required attribute
+    if(emailId === ''){
+        alert("Email address required.")
+    }
+    // else if(emailField === ){
+
+    // }
+}
+
+function fieldFocus(focusField){
+    // var fielddFocus = document.get
+    focusField.style.color = "red" 
+}
